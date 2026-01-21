1df060a37b3a024140570367b45a7795b88c0eb5
Vasco Gennari <vasco.gennari@gmail.com>
2026-01-21 17:00:50 +0100

    fix merge conflict

--- setup.py
+++ setup.py
@@ -13,15 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/simone-mastrogiovanni/icarogw',
     license='EUPL-1.2',
-<<<<<<< HEAD
     python_requires='>=3.11',
     packages=['icarogw'],
     install_requires=['bilby==2.6.0','mhealpy==0.3.6',
                      'ligo.skymap==2.4.0','mpmath==1.3.0','seaborn==0.13.2']
-=======
-    python_requires='>=3.12',
-    packages=['icarogw'],
-    install_requires=['bilby==2.6.0','mhealpy==0.3.6',
-                     'ligo.skymap==2.4.0','mpmath==1.3.0','seaborn==0.13.2','nessai-bilby==0.1.0.post0']
->>>>>>> main
 )
